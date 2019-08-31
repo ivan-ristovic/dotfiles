0c7efbeba44c483afecd543bdff92334f3ae0523
ivan-ristovic <ivo.ristovic@gmail.com>
2019-08-31 09:46:38 +0200

    Turns out I don't really like that autocompletion...

--- install/inst_zsh.sh
+++ install/inst_zsh.sh
@@ -16,7 +16,6 @@ rm install.sh
 rm ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 if ask "Change the theme?"; then
     TMP_CURDIR=`pwd`
