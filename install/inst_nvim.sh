b8010d4b78b7b4de09057f60edd05e69dcd65a9d
ivan-ristovic <ivan.ristovic95@gmail.com>
2021-12-02 22:29:30 +0100

    Use neovim properly; Add more plugins

--- install/inst_nvim.sh
+++ install/inst_nvim.sh
@@ -11,6 +11,8 @@ msg "Downloading Vundle ..."
 git clone https://github.com/VundleVim/Vundle.vim.git "$SETUP_HOME_DIR"/.config/nvim/bundle/Vundle.vim
 sudo chown $SETUP_USER:$SETUP_USER $SETUP_HOME_DIR/.config/nvim -R
 
+ln -s $SETUP_HOME_DIR/.vimrc $SETUP_HOME_DIR/.config/nvim/init.vim
+
 msg "Installing plugins ..."
-sudo vim +PluginInstall +qall
+vim +PluginInstall +qall
 
