1d98027d46853edcad3bccd4eea0cf0d46a95cc0
ivan-ristovic <ivan.ristovic95@gmail.com>
2021-12-26 12:21:20 +0100

    Implement package list imports

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/85cd90cd433749c7b7acbd7d019eedda)](https://app.codacy.com/manual/ivan-ristovic/dotfiles?utm_source=github.com&utm_medium=referral&utm_content=ivan-ristovic/dotfiles&utm_campaign=Badge_Grade_Dashboard)
 
-This repository contains my personal dotfiles and automated setup scripts for fresh Linux systems (primary goal for Arch and Ubuntu since that is what I use at the moment).
+This repository contains my personal dotfiles and automated setup scripts for fresh Linux systems (primary goal for Arch and Ubuntu since I use them at the moment).
+
+## How it works?
+
+Invoking `setup.sh` with username and package list will start the setup process as follows:
+- Identify user and environment (e.g. package manager command)
+- Setup packages from the package list
+- Link dotfiles from `dotfiles/` to user's home
+
+Packages are set up from the package lists in the following manner:
+- any lines beginning with a `#` sign are treated as comments
+- if a line begins with a `+` sign, a file whose name are the remaining line contents is included in the list
+- otherwise, the line represents a package name
+    - if there is a special setup script with such name in the `install/` directory, then invoke that script
+    - otherwise, attempt to install a package with such name using the default package manager
 
 Check out `install/` directory for more information on how to use the scripts for automatic mass package installations.
 
--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ fi
 msg "home dir: $SETUP_HOME_DIR"
 
 # Get install list path
-INSTALL_LIST="local_arch.lst"
+INSTALL_LIST="lst_local_arch"
 if [ $# -ge 2 ]; then
     if [ -f "$2" ]; then
         INSTALL_LIST="$2"
@@ -46,25 +46,36 @@ msg "Package manager installation command identified as: $PM"
 
 # Install packages
 suc "Checks completed successfully, starting installations ..."
-for entry in $(rm_comments "$INSTALL_LIST"); do
-    cd "install"
-    SETUP_SCRIPT="inst_$entry.sh"
-    AUR_PREFIX="aur_"
-    if [ -f "$SETUP_SCRIPT" ]; then
-        msg "Setting up via script : $entry"
-        source "$SETUP_SCRIPT" "$PM"
-    elif [[ "$entry" == "$AUR_PREFIX"* ]]; then
-        pkg=${entry#"$AUR_PREFIX"}
-        msg "Installing from AUR: $pkg"
-        inst_aur "$pkg"
-    else
-        msg "Installing package    : $entry"
-        inst "$PM" "$entry"
-    fi
-    cd "$ROOT_DIR"
-    suc "Finished processing $entry."
-    sleep 2
-done
+
+function process_list
+{
+    for entry in $(rm_comments "$1"); do
+        cd "install"
+        SETUP_SCRIPT="inst_$entry.sh"
+        AUR_PREFIX="aur_"
+        if [[ "$entry" == "+"* ]]; then
+            cd $ROOT_DIR
+            to_include=${entry#"+"}
+            msg "Importing setup script: $to_include"
+            process_list "$to_include"
+        elif [ -f "$SETUP_SCRIPT" ]; then
+            msg "Setting up via script : $entry"
+            source "$SETUP_SCRIPT" "$PM"
+        elif [[ "$entry" == "$AUR_PREFIX"* ]]; then
+            pkg=${entry#"$AUR_PREFIX"}
+            msg "Installing from AUR: $pkg"
+            inst_aur "$pkg"
+        else
+            msg "Installing package    : $entry"
+            inst "$PM" "$entry"
+        fi
+        cd "$ROOT_DIR"
+        suc "Finished processing $entry."
+        sleep 2
+    done
+}
+
+process_list "$INSTALL_LIST"
 suc "Installations finished."
 
 # Link dotfiles
