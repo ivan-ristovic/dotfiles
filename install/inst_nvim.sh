9039345d75d9a731b89c1895a70e85fc3bdab516
ivan-ristovic <work@ristovic.net>
2022-03-28 00:30:43 +0200

    Migrate to vim-plug, add completion

--- install/inst_nvim.sh
+++ install/inst_nvim.sh
@@ -8,11 +8,13 @@ inst $@ curl
 inst $@ neovim
 
 msg "Downloading Vundle ..."
-git clone https://github.com/VundleVim/Vundle.vim.git "$SETUP_HOME_DIR"/.config/nvim/bundle/Vundle.vim
-sudo chown $SETUP_USER:$SETUP_USER $SETUP_HOME_DIR/.config/nvim -R
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$SETUP_HOME_DIR/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ln -s $SETUP_HOME_DIR/.vimrc $SETUP_HOME_DIR/.config/nvim/init.vim
 
 msg "Installing plugins ..."
-vim +PluginInstall +qall
+nvim +PlugInstall +qall
+
+inst $@ python-pip
+pip install pynvim
 
