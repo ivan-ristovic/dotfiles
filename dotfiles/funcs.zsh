288d2eda42620738161ed6e3255e85a1b014b560
ivan-ristovic <work@ristovic.net>
2022-05-30 15:35:08 +0200

    Add ask function

--- dotfiles/funcs.zsh
+++ dotfiles/funcs.zsh
@@ -13,3 +13,41 @@ function beep_v() {
 function mp3gain_all() {
    fd -g \*.mp3 -X mp3gain -g $@ 
 }
+
+# http://djm.me/ask
+function ask()
+{
+    while true; do
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read REPLY </dev/tty
+        sleep 1
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY="$default"
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
