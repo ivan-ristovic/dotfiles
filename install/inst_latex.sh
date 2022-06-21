03ae2dbaabe651a5dc81bf73b12c17b68a25257d
ivan-ristovic <work@ristovic.net>
2022-06-21 20:51:57 +0200

    Minor fixes

--- install/inst_latex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "utils.sh"
-
-inst $@ texlive-latex-recommended texlive-lang-other texlive-lang-cyrillic texlive-luatex
--- install/inst_qt5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "utils.sh"
-
-inst $@ qt5-default
-inst $@ qtcreator
-inst $@ qt5-doc qt5-doc-html qtbase5-doc-html
-msg "Executing ldconfig ..."
-/sbin/ldconfig -v > /dev/null
