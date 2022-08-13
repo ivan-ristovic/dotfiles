b8fd68ae8e8e02ec880350afa6157071bbaa450c
ivan-ristovic <work@ristovic.net>
2022-08-13 14:38:24 +0200

    Unify zsh setup

--- install/inst_zsh.sh
+++ install/inst_zsh.sh
@@ -9,10 +9,8 @@ inst $@ curl
 
 msg "Installing powerline ..."
 pip install powerline-status
-inst $@ fonts-powerline
 
 msg "Installing oh-my-zsh ..."
-
 sudo -u $SETUP_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 rm "$SETUP_HOME_DIR"/.zshrc
 
@@ -25,6 +23,5 @@ sudo -u $SETUP_USER git clone --depth=1 https://github.com/romkatv/powerlevel10k
 msg "Downloading zplug ..."
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-rm $SETUP_HOME_DIR/.bashrc
 sudo -u $SETUP_USER chsh -s "$(which zsh)"
 
