f01fc52c00cd02472147c0fc92230d1e8fadfb05
ivan-ristovic <work@ristovic.net>
2022-11-27 00:54:12 +0100

    Fix vim-plug install

--- install/inst_nvim.sh
+++ install/inst_nvim.sh
@@ -8,10 +8,10 @@ inst $@ curl
 inst $@ neovim
 
 msg "Downloading vim-plug ..."
-curl -fLo ${SETUP_HOME_DIR}/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+as_user curl -fLo ${SETUP_HOME_DIR}/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 msg "Installing plugins ..."
-nvim +PlugInstall +qall
+as_user nvim +PlugInstall +qall
 
 inst $@ python-pip
 as_user pip3 install pynvim
