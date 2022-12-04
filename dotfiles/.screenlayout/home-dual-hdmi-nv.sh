8fc0cad93589f1c22c98e5de976234fa7ae6b381
ivan-ristovic <work@ristovic.net>
2022-12-04 12:55:40 +0100

    Update layouts

--- dotfiles/.screenlayout/home-dual-hdmi-nv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --rate 144.01 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rate 60.02 --rotate normal
--- dotfiles/.screenlayout/home-dual.sh
+++ dotfiles/.screenlayout/home-dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --mode 1920x1080 --rate 60.02 --pos 1920x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --rate 144.01 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
