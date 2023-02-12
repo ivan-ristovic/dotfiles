c1ce3ca8c96965cc1ecaa36313749ca9dea1e0b2
ivan-ristovic <work@ristovic.net>
2023-02-12 17:19:18 +0100

    Update screen layouts for nvidia layouts

---
+++ dotfiles/.screenlayout/default-nv-dual.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --rate 144.01 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+feh --bg-fill ~/.config/i3/wp.png
