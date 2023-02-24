d263de6379e56a58897deffe8797b548b7ff4d0d
ivan-ristovic <work@ristovic.net>
2023-02-24 21:44:19 +0100

    Remove deprecated nvim-tree funcs

--- dotfiles/.config/nvim/lua/conf/filetree.lua
+++ dotfiles/.config/nvim/lua/conf/filetree.lua
@@ -10,8 +10,24 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function open_nvim_tree(data)
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if real_file or not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 nvim_tree.setup {
-  open_on_setup = true,
   update_focused_file = {
     enable = true,
     update_cwd = true,
