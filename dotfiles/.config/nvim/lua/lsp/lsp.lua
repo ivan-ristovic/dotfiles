0a95adfe857d4080916fe5b55e81995943526c08
ivan-ristovic <work@ristovic.net>
2023-02-24 19:08:18 +0100

    Remove deprecations in lua lsp

--- dotfiles/.config/nvim/lua/lsp/lsp.lua
+++ dotfiles/.config/nvim/lua/lsp/lsp.lua
@@ -52,7 +52,7 @@ local servers = {
   'clangd',
   'rust_analyzer',
   'pyright',
-  'sumneko_lua',
+  'lua_ls',
 }
 
 -- Ensure the servers above are installed
@@ -81,7 +81,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
