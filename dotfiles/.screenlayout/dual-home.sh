b1da6bf3654e76ed4f05edce985f48bce8156886
ivan-ristovic <work@ristovic.net>
2023-03-11 08:50:12 +0100

    Update screenlayouts

--- dotfiles/.screenlayout/dual-home.sh
+++ dotfiles/.screenlayout/dual-home.sh
@@ -3,7 +3,7 @@
 if glxinfo | grep "OpenGL vendor string" | grep NVIDIA ; then
     xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off
 else 
-    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off
+    xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --rate 144.01 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal
 fi
 
 feh --bg-fill ~/.config/i3/wp.png
--- dotfiles/.screenlayout/home-dual.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --rate 60.02 --pos 1920x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --rate 144.01 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
-feh --bg-fill ~/.config/i3/wp.png
