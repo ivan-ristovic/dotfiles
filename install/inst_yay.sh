1fc70485eebe2d1908ef6a83ab003a3f0d674b73
ivan-ristovic <work@ristovic.net>
2023-04-13 00:04:38 +0200

    [install] yay: improvements

--- install/inst_yay.sh
+++ install/inst_yay.sh
@@ -2,12 +2,12 @@
 
 source "utils.sh"
 
-inst_silent $@ yay
-if [ $? -ne 0 ]; then
+# Check if yay is already available in the repository
+if inst_silent $@ yay ; then
     inst $@ base-devel
     yaydir=/opt/yay
-    mkdir $yaydir
-    chown user:user $yaydir
+    mkdir -p $yaydir
+    chown $SETUP_USER:$SETUP_USER $yaydir
     gcl https://aur.archlinux.org/yay-bin.git $yaydir
     pushd $yaydir
     as_user makepkg -si --noconfirm --needed
