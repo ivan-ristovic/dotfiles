cb8d29332b715125ec6768325eefb68faae4dad0
ivan-ristovic <work@ristovic.net>
2023-07-19 22:18:03 +0200

    [setup] better logging

--- install/utils.sh
+++ install/utils.sh
@@ -4,27 +4,38 @@ PROMPT=">"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;36m'
+BOLD='\033[1m'
 NC='\033[0m'
 
 function suc ()
 {
-    echo -e "${GREEN}${PROMPT} suc: ${NC}$@"
+    # echo -e "${GREEN}${PROMPT} suc: ${NC}$@"
+    tprint "suc" $GREEN $*
 }
 
 function msg ()
 {
-    echo -e "${YELLOW}${PROMPT} inf: ${NC}$@"
+    # echo -e "${YELLOW}${PROMPT} inf: ${NC}$@"
+    tprint "msg" $YELLOW $*
 }
 
 function err ()
 {
-    echo -e "${RED}${PROMPT} err: ${NC}$@"
+    # echo -e "${RED}${PROMPT} err: ${NC}$@"
+    tprint "err" $RED $*
 }
 
 function fat ()
 {
     err $@
     exit 1
+}
+
+function tprint ()
+{
+    echo -en "$BOLD[$(date +%T)]$2$PROMPT $1: $NC"
+    shift 2
+    echo -e "$@"
 }
 
 function is_installed ()
@@ -63,7 +74,8 @@ function inst ()
 {
     if ! sudo $@; then
         err "An error occurred while installing: $@"
-        sleep 5
+        sleep 1
+        return 1
     fi
 }
 
@@ -76,7 +88,8 @@ function inst_aur ()
 {
     if ! echo y | sudo -u $SETUP_USER yay -S --needed --noconfirm --noprovides --answerdiff None --answerclean None --mflags "--noconfirm --needed" $@; then
         err "An error occurred while installing from AUR: $@"
-        sleep 5
+        sleep 1
+        return 1
     fi
 }
 
@@ -103,6 +116,7 @@ function pm_cmd ()
     do
         if [[ -f $f ]]; then
             echo "${osinfo["$f"]}"
+            return
         fi
     done
 }
@@ -120,6 +134,7 @@ function pm_uninst_cmd ()
     do
         if [[ -f $f ]]; then
             echo "${osinfo["$f"]}"
+            return 
         fi
     done
 }
