da361039c6e46095113b21a34bf886cd20e98982
ivan-ristovic <work@ristovic.net>
2023-07-22 16:00:22 +0200

    [autorandr] add single nv layout

---
+++ dotfiles/.screenlayout/mon-nv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --rate 144 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --off
