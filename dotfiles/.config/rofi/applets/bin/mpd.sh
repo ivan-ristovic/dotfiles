cec359630ff503d3fe8d945b549ba1a7796619a0
ivan-ristovic <work@ristovic.net>
2023-07-25 19:58:09 +0200

    [rofi] update glyphs

--- dotfiles/.config/rofi/applets/bin/mpd.sh
+++ dotfiles/.config/rofi/applets/bin/mpd.sh
@@ -12,51 +12,51 @@ theme="$type/$style"
 # Theme Elements
 status="`mpc status`"
 if [[ -z "$status" ]]; then
-	prompt='Offline'
-	mesg="MPD is Offline"
+    prompt='Offline'
+    mesg="MPD is Offline"
 else
-	prompt="`mpc -f "%artist%" current`"
+    prompt="`mpc -f "%artist%" current`"
     if [[ -z "$prompt" ]]; then
         mesg="`mpc status | head -1` :: `mpc status | grep "#" | awk '{print $3}'`"
-	else
+    else
         mesg="`mpc -f "%title%" current` :: `mpc status | grep "#" | awk '{print $3}'`"
     fi
 fi
 
 if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	list_col='1'
-	list_row='7'
+    list_col='1'
+    list_row='7'
 elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='7'
-	list_row='1'
+    list_col='7'
+    list_row='1'
 fi
 
 # Options
 layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
 if [[ "$layout" == 'NO' ]]; then
-	if [[ ${status} == *"[playing]"* ]]; then
-		option_1=" Pause"
-	else
-		option_1="契 Play"
-	fi
-	option_2="栗 Stop"
-	option_3="玲 Previous"
-	option_4="怜 Next"
-	option_5="凌 Repeat"
-	option_6="列 Random"
-	option_7=" Seek to start"
+    if [[ ${status} == *"[playing]"* ]]; then
+        option_1=" Pause"
+    else
+        option_1=" Play"
+    fi
+    option_2=" Stop"
+    option_3=" Previous"
+    option_4=" Next"
+    option_5=" Repeat"
+    option_6=" Random"
+    option_7=" Seek to start"
 else
-	if [[ ${status} == *"[playing]"* ]]; then
-		option_1=""
-	else
-		option_1="契"
-	fi
-	option_2="栗"
-	option_3="玲"
-	option_4="怜"
-	option_5="凌"
-	option_6="列"
-	option_7=""
+    if [[ ${status} == *"[playing]"* ]]; then
+        option_1=""
+    else
+        option_1=""
+    fi
+    option_2=""
+    option_3=""
+    option_4=""
+    option_5=""
+    option_6=""
+    option_7=""
 fi
 
 # Toggle Actions
@@ -81,62 +81,62 @@ fi
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		${active} ${urgent} \
-		-markup-rows \
-		-theme ${theme}
+    rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+        -theme-str 'textbox-prompt-colon {str: "";}' \
+        -dmenu \
+        -p "$prompt" \
+        -mesg "$mesg" \
+        ${active} ${urgent} \
+        -markup-rows \
+        -theme ${theme}
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7" | rofi_cmd
+    echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		mpc -q toggle
-	elif [[ "$1" == '--opt2' ]]; then
-		mpc -q stop
-	elif [[ "$1" == '--opt3' ]]; then
-		mpc -q prev && notify-send -u low -t 2000 " `mpc current`"
-	elif [[ "$1" == '--opt4' ]]; then
-		mpc -q next && notify-send -u low -t 2000 " `mpc current`"
-	elif [[ "$1" == '--opt5' ]]; then
-		mpc -q repeat
-	elif [[ "$1" == '--opt6' ]]; then
-		mpc -q random && notify-send -u low -t 2000 " `mpc current`"
-	elif [[ "$1" == '--opt7' ]]; then
-		mpc -q seek 0%
-	fi
+    if [[ "$1" == '--opt1' ]]; then
+        mpc -q toggle
+    elif [[ "$1" == '--opt2' ]]; then
+        mpc -q stop
+    elif [[ "$1" == '--opt3' ]]; then
+        mpc -q prev && notify-send -u low -t 2000 " `mpc current`"
+    elif [[ "$1" == '--opt4' ]]; then
+        mpc -q next && notify-send -u low -t 2000 " `mpc current`"
+    elif [[ "$1" == '--opt5' ]]; then
+        mpc -q repeat
+    elif [[ "$1" == '--opt6' ]]; then
+        mpc -q random && notify-send -u low -t 2000 " `mpc current`"
+    elif [[ "$1" == '--opt7' ]]; then
+        mpc -q seek 0%
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $option_1)
-		run_cmd --opt1
+        run_cmd --opt1
         ;;
     $option_2)
-		run_cmd --opt2
+        run_cmd --opt2
         ;;
     $option_3)
-		run_cmd --opt3
+        run_cmd --opt3
         ;;
     $option_4)
-		run_cmd --opt4
+        run_cmd --opt4
         ;;
     $option_5)
-		run_cmd --opt5
+        run_cmd --opt5
         ;;
     $option_6)
-		run_cmd --opt6
+        run_cmd --opt6
         ;;
     $option_7)
-		run_cmd --opt7
+        run_cmd --opt7
         ;;
 esac
--- dotfiles/.config/rofi/applets/bin/powermenu.sh
+++ dotfiles/.config/rofi/applets/bin/powermenu.sh
@@ -14,11 +14,11 @@ prompt="`hostname`"
 mesg="Uptime : `uptime -p | sed -e 's/up //g'`"
 
 if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	list_col='1'
-	list_row='7'
+    list_col='1'
+    list_row='7'
 elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='7'
-	list_row='1'
+    list_col='7'
+    list_row='1'
 fi
 
 test -f /run/systemd/shutdown/scheduled
@@ -27,130 +27,130 @@ is_shutdown=$?
 # Options
 layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
 if [[ "$layout" == 'NO' ]]; then
-	option_1=" Lock"
-	option_2=" Logout"
-	option_3=" Suspend"
-	option_4=" Hibernate"
-	option_5="󰜉 Reboot"
-	option_6=" Shutdown"
-	if [ $is_shutdown -eq 0 ] ; then
-		option_7="󰜺 Cancel scheduled shutdown"
-	else 
-		option_7=" Shutdown in 1h"
-	fi
-	yes=' Yes'
-	no=' No'
+    option_1=" Lock"
+    option_2="󰍃 Logout"
+    option_3=" Suspend"
+    option_4=" Hibernate"
+    option_5=" Reboot"
+    option_6=" Shutdown"
+    if [ $is_shutdown -eq 0 ] ; then
+        option_7="󰜺 Cancel scheduled shutdown"
+    else 
+        option_7="󰄉 Shutdown in 1h"
+    fi
+    yes=' Yes'
+    no=' No'
 else
-	option_1=""
-	option_2=""
-	option_3=""
-	option_4=""
-	option_5="󰜉"
-	option_6=""
-	if [ $is_shutdown -eq 0 ] ; then
-		option_7="󰜺"
-	else 
-		option_7=""
-	fi
-	yes=''
-	no=''
+    option_1=""
+    option_2="󰍃"
+    option_3=""
+    option_4=""
+    option_5=""
+    option_6=""
+    if [ $is_shutdown -eq 0 ] ; then
+        option_7="󰜺"
+    else 
+        option_7="󰄉"
+    fi
+    yes=''
+    no=''
 fi
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: " ";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
+    rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+        -theme-str 'textbox-prompt-colon {str: " ";}' \
+        -dmenu \
+        -p "$prompt" \
+        -mesg "$mesg" \
+        -markup-rows \
+        -theme ${theme}
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7" | rofi_cmd
+    echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7" | rofi_cmd
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${theme}
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+        -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ${theme}
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Confirm and execute
-confirm_run () {	
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
+confirm_run () {    
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
         ${1} && ${2} && ${3}
     else
         exit
-    fi	
+    fi  
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		betterlockscreen -l
-	elif [[ "$1" == '--opt2' ]]; then
-		confirm_run 'kill -9 -1'
-	elif [[ "$1" == '--opt3' ]]; then
-		confirm_run 'mpc -q pause' 'amixer set Master mute' 'systemctl suspend'
-	elif [[ "$1" == '--opt4' ]]; then
-		confirm_run 'systemctl hibernate'
-	elif [[ "$1" == '--opt5' ]]; then
-		confirm_run 'systemctl reboot'
-	elif [[ "$1" == '--opt6' ]]; then
-		confirm_run 'systemctl poweroff'
-	elif [[ "$1" == '--opt7' ]]; then
-		if [ $is_shutdown -eq 0 ] ; then
-			confirm_run 'shutdown -c'
-		else 
-			confirm_run 'shutdown -h +60'
-		fi
-	fi
+    if [[ "$1" == '--opt1' ]]; then
+        betterlockscreen -l
+    elif [[ "$1" == '--opt2' ]]; then
+        confirm_run 'kill -9 -1'
+    elif [[ "$1" == '--opt3' ]]; then
+        confirm_run 'mpc -q pause' 'amixer set Master mute' 'systemctl suspend'
+    elif [[ "$1" == '--opt4' ]]; then
+        confirm_run 'systemctl hibernate'
+    elif [[ "$1" == '--opt5' ]]; then
+        confirm_run 'systemctl reboot'
+    elif [[ "$1" == '--opt6' ]]; then
+        confirm_run 'systemctl poweroff'
+    elif [[ "$1" == '--opt7' ]]; then
+        if [ $is_shutdown -eq 0 ] ; then
+            confirm_run 'shutdown -c'
+        else 
+            confirm_run 'shutdown -h +60'
+        fi
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $option_1)
-		run_cmd --opt1
+        run_cmd --opt1
         ;;
     $option_2)
-		run_cmd --opt2
+        run_cmd --opt2
         ;;
     $option_3)
-		run_cmd --opt3
+        run_cmd --opt3
         ;;
     $option_4)
-		run_cmd --opt4
+        run_cmd --opt4
         ;;
     $option_5)
-		run_cmd --opt5
+        run_cmd --opt5
         ;;
     $option_6)
-		run_cmd --opt6
+        run_cmd --opt6
         ;;
     $option_7)
-		run_cmd --opt7
-		if [ $is_shutdown -eq 0 ] ; then
-			notify-send "Scheduled shutdown cancelled"
-		else 
-			notify-send "Powering off in 1h"
-		fi
+        run_cmd --opt7
+        if [ $is_shutdown -eq 0 ] ; then
+            notify-send "Scheduled shutdown cancelled"
+        else 
+            notify-send "Powering off in 1h"
+        fi
         ;;
 esac
 
