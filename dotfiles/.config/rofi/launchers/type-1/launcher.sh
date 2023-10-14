7aaed9f85c4120805ab95e04cbdda11623cfe5b3
ivan-ristovic <work@ristovic.net>
2023-10-14 15:12:02 +0200

    [rofi] cleanup

--- dotfiles/.config/rofi/launchers/type-1/launcher.sh
+++ dotfiles/.config/rofi/launchers/type-1/launcher.sh
@@ -9,7 +9,6 @@
 #
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
-## style-11    style-12    style-13    style-14    style-15
 
 dir="$HOME/.config/rofi/launchers/type-1"
 theme='style-10'
