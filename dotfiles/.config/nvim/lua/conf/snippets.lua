795108e68d21daf72031ad68f93cea0dbfd50918
ivan-ristovic <work@ristovic.net>
2024-02-01 23:44:11 +0100

    [nvim] snippet support

--- dotfiles/.config/nvim/lua/conf/snippets.lua
+++ dotfiles/.config/nvim/lua/conf/snippets.lua
@@ -1,6 +1,10 @@
-local status_ok, luasnip = pcall(require, "luasnip.loaders.from_snipmate")
-if not status_ok then
-  return
+local status1_ok, ls = pcall(require, "luasnip")
+local status2_ok, luasnip = pcall(require, "luasnip.loaders.from_snipmate")
+if not status1_ok or not status2_ok then
+    return
 end
 
+ls.filetype_extend("all", { "_" })
+
 luasnip.lazy_load()
+luasnip.load({ paths = { "./snippets" } })
