291c9a603cbf8b6247fa3956bb3bac26328568e9
ivan-ristovic <work@ristovic.net>
2024-04-03 23:27:38 +0200

    [shlib] refactor log

--- include/log.sh
+++ include/log.sh
@@ -3,8 +3,8 @@
 SHLIB_FMT_PROMPT=">"
 SHLIB_FMT_C_R='\033[0;31m'
 SHLIB_FMT_C_G='\033[0;32m'
+SHLIB_FMT_C_Y='\033[0;33m'
 SHLIB_FMT_C_B='\033[0;34m'
-SHLIB_FMT_C_Y='\033[0;36m'
 SHLIB_FMT_S_B='\033[1m'
 SHLIB_FMT_CLR='\033[0m'
 
@@ -16,44 +16,46 @@ function log::exec ()
 
 function log::suc ()
 {
-    if [ -z "$SHLIB_FMT_TIME" ]; then
-        echo -e "${SHLIB_FMT_C_G}${SHLIB_FMT_PROMPT} suc: ${SHLIB_FMT_CLR}$@"
-    else
-        log::_tprint "suc" $SHLIB_FMT_C_G $*
-    fi
+    log::log "suc" "$SHLIB_FMT_C_G" "$*"
 }
 
 function log::msg ()
 {
-    if [ -z "$SHLIB_FMT_TIME" ]; then
-        echo -e "${SHLIB_FMT_C_Y}${SHLIB_FMT_PROMPT} inf: ${SHLIB_FMT_CLR}$@"
-    else
-        log::_tprint "msg" $SHLIB_FMT_C_Y $*
-    fi
+    log::log "inf" "$SHLIB_FMT_C_B" "$*"
 }
 
 function log::wrn ()
 {
-    if [ -z "$SHLIB_FMT_TIME" ]; then
-        echo -e "${SHLIB_FMT_C_R}${SHLIB_FMT_PROMPT} WRN: ${SHLIB_FMT_CLR}$@"
-    else
-        log::_tprint "WRN" $SHLIB_FMT_C_R $*
-    fi
+    log::log "WRN" "$SHLIB_FMT_C_Y" "$*"
 }
 
 function log::err ()
 {
+    log::log "ERR" "$SHLIB_FMT_C_R" "$*"
+}
+
+function log::log ()
+{
+    local level="$1"
+    local color="$2"
+    shift 2
     if [ -z "$SHLIB_FMT_TIME" ]; then
-        echo -e "${SHLIB_FMT_C_R}${SHLIB_FMT_PROMPT} err: ${SHLIB_FMT_CLR}$@"
+        echo -e "${color}${SHLIB_FMT_PROMPT} $level: ${SHLIB_FMT_CLR}$*"
     else
-        log::_tprint "err" $SHLIB_FMT_C_R $*
+        echo -en "${SHLIB_FMT_S_B}[$(date +%T)]${color}${SHLIB_FMT_PROMPT} $level: ${SHLIB_FMT_CLR}"
+        echo -e "$@"
     fi
 }
 
-function log::_tprint ()
+function log::stacktrace () 
 {
-    echo -en "$SHLIB_FMT_S_B[$(date +%T)]$2$SHLIB_FMT_PROMPT $1: $SHLIB_FMT_CLR"
-    shift 2
-    echo -e "$@"
+    local frame=0 line func source n=0
+    while caller "$frame"; do
+        ((frame++))
+    done | while read -r line func source; do
+        ((n++ == 0)) && {
+            log::log "trace" "red_i"
+        }
+        printf '%4s at %s\n' " " "$func ($source:$line)"
+    done
 }
-
