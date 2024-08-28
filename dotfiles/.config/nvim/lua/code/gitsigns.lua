17fe555250d4a7b264931617989782e918f6cf7b
Ivan Ristovic <work@ristovic.net>
2024-08-28 23:49:58 +0200

    [nvim] update gitsigns config

--- dotfiles/.config/nvim/lua/code/gitsigns.lua
+++ dotfiles/.config/nvim/lua/code/gitsigns.lua
@@ -5,13 +5,14 @@ end
 
 gitsigns.setup {
   signs = {
-    add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-    change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-    untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '│' },
+    topdelete    = { text = '│' },
+    changedelete = { text = '│' },
+    untracked    = { text = '┆' },
   },
+  signs_staged_enable = true,
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -20,6 +21,7 @@ gitsigns.setup {
     interval = 5000,
     follow_files = true
   },
+  auto_attach = true,
   attach_to_untracked = true,
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
@@ -40,9 +42,6 @@ gitsigns.setup {
     relative = 'cursor',
     row = 0,
     col = 1
-  },
-  yadm = {
-    enable = false
   },
 }
 
