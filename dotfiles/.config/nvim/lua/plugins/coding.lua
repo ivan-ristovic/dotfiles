c6137813bc3989c89963e1f7481dd1fc09bb325d
Ivan Ristovic <ivan.ristovic@oracle.com>
2024-08-29 11:37:25 +0200

    Init

--- dotfiles/.config/nvim/lua/plugins/coding.lua
+++ dotfiles/.config/nvim/lua/plugins/coding.lua
@@ -68,48 +68,48 @@ return {
   },
 
   -- Split/Join blocks of code
-  {
-    'Wansmer/treesj',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    opts = {
-
-      -- Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
-      use_default_keymaps = false,
-
-      -- Node with syntax error will not be formatted
-      check_syntax_error = true,
-
-      -- If line after join will be longer than max value, node will not be formatted
-      max_join_length = 120,
-
-      ---Cursor behavior:
-      ---hold - cursor follows the node/place on which it was called
-      ---start - cursor jumps to the first symbol of the node being formatted
-      ---end - cursor jumps to the last symbol of the node being formatted
-      cursor_behavior = 'hold',
-
-      -- Notify about possible problems or not
-      notify = true,
-
-      -- Use `dot` for repeat action
-      dot_repeat = true,
-
-      -- Callback for treesj error handler. func (err_text, level, ...other_text)
-      on_error = nil,
-
-      -- Presets for languages
-      -- langs = {}, -- See the default presets in lua/treesj/langs
-    },
-    keys = {
-      { "<leader>z", require('treesj').toggle, desc = "Toggle multiline object split" },
-      {
-        "<leader>Z",
-        function()
-          require('treesj').toggle({ split = { recursive = true } })
-        end,
-        desc = "Toggle multiline object split"
-      },
-    }
-  },
+--  {
+--    'Wansmer/treesj',
+--    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+--    opts = {
+--
+--      -- Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+--      use_default_keymaps = false,
+--
+--      -- Node with syntax error will not be formatted
+--      check_syntax_error = true,
+--
+--      -- If line after join will be longer than max value, node will not be formatted
+--      max_join_length = 120,
+--
+--      ---Cursor behavior:
+--      ---hold - cursor follows the node/place on which it was called
+--      ---start - cursor jumps to the first symbol of the node being formatted
+--      ---end - cursor jumps to the last symbol of the node being formatted
+--      cursor_behavior = 'hold',
+--
+--      -- Notify about possible problems or not
+--      notify = true,
+--
+--      -- Use `dot` for repeat action
+--      dot_repeat = true,
+--
+--      -- Callback for treesj error handler. func (err_text, level, ...other_text)
+--      on_error = nil,
+--
+--      -- Presets for languages
+--      -- langs = {}, -- See the default presets in lua/treesj/langs
+--    },
+--    keys = {
+--      { "<leader>z", require('treesj').toggle, desc = "Toggle multiline object split" },
+--      {
+--        "<leader>Z",
+--        function()
+--          require('treesj').toggle({ split = { recursive = true } })
+--        end,
+--        desc = "Toggle multiline object split"
+--      },
+--    }
+--  },
 
 }
