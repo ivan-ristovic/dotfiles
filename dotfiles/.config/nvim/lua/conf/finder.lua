22ea30d45a4163f969bf5fe106244d1e4ba2f623
Ivan Ristovic <work@ristovic.net>
2024-10-14 22:08:48 +0200

    [nvim] refactor telescope cfg

--- dotfiles/.config/nvim/lua/conf/finder.lua
+++ dotfiles/.config/nvim/lua/conf/finder.lua
@@ -4,12 +4,13 @@ if not status_ok then
 end
 
 local actions = require "telescope.actions"
+local builtin = require "telescope.builtin"
 
 telescope.setup {
   defaults = {
 
-    prompt_prefix = " ",
-    selection_caret = "",
+    prompt_prefix = "  ",
+    selection_caret = " ",
     path_display = { "smart" },
 
     mappings = {
@@ -104,22 +105,22 @@ telescope.setup {
 pcall(telescope.load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = true,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>su', require('telescope.builtin').lsp_references, { desc = '[S]earch [U]sages' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').lsp_definitions, { desc = '[S]earch [D]efinitions' })
-vim.keymap.set('n', '<leader>si', require('telescope.builtin').lsp_implementations, { desc = '[S]earch [I]mplementations' })
+vim.keymap.set('n', '<leader>sf',  builtin.find_files,          { desc = '[S]earch [F]iles'           })
+vim.keymap.set('n', '<leader>sh',  builtin.help_tags,           { desc = '[S]earch [H]elp'            })
+vim.keymap.set('n', '<leader>sw',  builtin.grep_string,         { desc = '[S]earch current [W]ord'    })
+vim.keymap.set('n', '<leader>sg',  builtin.live_grep,           { desc = '[S]earch by [G]rep'         })
+vim.keymap.set('n', '<leader>sd',  builtin.diagnostics,         { desc = '[S]earch [D]iagnostics'     })
+vim.keymap.set('n', '<leader>su',  builtin.lsp_references,      { desc = '[S]earch [U]sages'          })
+vim.keymap.set('n', '<leader>sd',  builtin.lsp_definitions,     { desc = '[S]earch [D]efinitions'     })
+vim.keymap.set('n', '<leader>si',  builtin.lsp_implementations, { desc = '[S]earch [I]mplementations' })
 
