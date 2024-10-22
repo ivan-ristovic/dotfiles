7a6906bd84cf7ab67a029681b46247415330ed43
Ivan Ristovic <work@ristovic.net>
2024-10-22 23:20:48 +0200

    [nvim] setup nvim-surround

--- dotfiles/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/.config/nvim/lua/plugins/misc.lua
@@ -30,6 +30,11 @@ return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   },
 
   -- Undo history
