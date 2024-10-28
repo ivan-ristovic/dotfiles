3066a64065c6303bde1572dcdfa7cf266c1a8c9a
Ivan Ristovic <work@ristovic.net>
2024-10-28 21:20:10 +0100

    [nvim] add trouble

---
+++ dotfiles/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,42 @@
+-- Diagnostics, LSP, qfix lists
+return {
+
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<C-X>a",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (all buffers)",
+      },
+      {
+        "<C-X>c",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Diagnostics (current buffer)",
+      },
+      {
+        "<leader>S",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Diagnostics Focus (Trouble)",
+      },
+      {
+        "<C-X>l",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "Diagnostics LSP Definitions / references / ...",
+      },
+      {
+        "<C-X>L",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Diagnostics Location List",
+      },
+      {
+        "<C-X>q",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Diagnostics Quickfix List (Trouble)",
+      },
+    },
+  }
+
+}
