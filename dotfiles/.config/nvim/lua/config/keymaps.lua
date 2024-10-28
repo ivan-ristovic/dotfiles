018b61036860e2b710ceea2f5716f7d06fdb49c3
Ivan Ristovic <work@ristovic.net>
2024-10-28 21:20:02 +0100

    [nvim] update qol keymaps

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -15,7 +15,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 --   command_mode = "c",
 
 -- Remap increment to C-i 
-U.keymap("n", "<C-i>", "<C-a>", "Increment")
+U.keymap("n", "<C-i>", "<C-a>", "Increment selection")
 
 -- Common actions
 U.keymap("n", "<leader>w", "<cmd>w<cr>", "Save")
@@ -23,12 +23,18 @@ U.keymap("n", "<leader>s", "<cmd>wa<cr>", "Save all")
 U.keymap("n", "<leader>x", "<cmd>x<cr>", "Save and exit")
 U.keymap("n", "<leader>q", "<cmd>q!<cr>", "Exit without saving")
 U.keymap("n", "<leader>z", "<cmd>qa!<cr>", "Exit all without saving")
-U.keymap("n", "<A-Left>", ":bprevious<CR>")
-U.keymap("n", "<A-Right>", ":bnext<CR>")
 
 -- Shell-like moving to begin/end of line
-U.keymap("n", "<C-a>", "^", "Go to beginning of line")
+U.keymap("n", "ga", "^", "Go to beginning of line")
+U.keymap("n", "ge", "$", "Go to end of line")
+U.keymap("n", "<C-a", "^", "Go to beginning of line")
 U.keymap("n", "<C-e>", "$", "Go to end of line")
+
+-- Jump motions
+U.keymap("n", "<C-A-Left>", "<C-O>", "[G]o to [P]revious Cursor Position")
+U.keymap("n", "<C-A-Right", "<C-I>", "[G]o to [N]ext Cursor Position")
+U.keymap("n", "gp", "<C-O>", "[G]o to [P]revious Cursor Position")
+U.keymap("n", "gn", "<C-I>", "[G]o to [N]ext Cursor Position")
 
 -- Word wrap
 U.keymap("n", "<A-z>", ":set wrap!<cr>", "Word wrap")
@@ -36,10 +42,10 @@ U.keymap_expr('n', '<Up>', "v:count == 0 ? 'gk' : 'k'", "Wrap-aware navigation")
 U.keymap_expr('n', '<Down>', "v:count == 0 ? 'gj' : 'j'", "Wrap-aware navigation")
 
 -- Resize with arrows
-U.keymap("n", "<C-A-Up>", ":resize -2<cr>", "Resize")
-U.keymap("n", "<C-A-Down>", ":resize +2<cr>", "Resize")
-U.keymap("n", "<C-A-Left>", ":vertical resize -2<cr>", "Resize")
-U.keymap("n", "<C-A-Right>", ":vertical resize +2<cr>", "Resize")
+--[[ U.keymap("n", "<C-A-Up>", ":resize -2<cr>", "Resize") ]]
+--[[ U.keymap("n", "<C-A-Down>", ":resize +2<cr>", "Resize") ]]
+--[[ U.keymap("n", "<C-A-Left>", ":vertical resize -2<cr>", "Resize") ]]
+--[[ U.keymap("n", "<C-A-Right>", ":vertical resize +2<cr>", "Resize") ]]
 
 -- Navigate buffers/windows
 -- Note: <C-PgUp>/gT and <C-PgDown>/gt are default tabpage navigation keymaps!
@@ -47,8 +53,8 @@ U.keymap("n", "<C-A-q>", ":tabclose!<cr>")
 U.keymap("n", "<C-A-c>", ":tabedit<cr>")
 U.keymap("n", "<leader>hs", ":hs<cr>")
 U.keymap("n", "<leader>vs", ":vs<cr>")
---[[ U.keymap("n", "<A-Left>", ":bprevious<cr>") ]]
---[[ U.keymap("n", "<A-Right>", ":bnext<cr>") ]]
+U.keymap("n", "<A-Left>", ":bprevious<cr>")
+U.keymap("n", "<A-Right>", ":bnext<cr>")
 
 -- Move selection up/down
 U.keymap("n", "<A-Up>", ":m -2<cr>==", "Move selection up")
--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -82,11 +82,11 @@ return {
 
         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
         nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-        nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+        nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+        nmap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
         nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
         nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -96,7 +96,6 @@ return {
 
         -- Lesser used LSP functionality
         nmap('<leader>l', vim.diagnostic.open_float)
-        nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
         nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
         nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
         nmap('<leader>wl', function()
