40312f170fe5d4cd3718b989ab716f39979221bf
Ivan Ristovic <work@ristovic.net>
2024-10-29 18:26:17 +0100

    [nvim] add context ts

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -77,6 +77,26 @@ return {
       })
 
     end    -- end config
-  }
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    event = "VeryLazy",
+    opts = {
+      enable = true,
+      max_lines = 1,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = 'outer',
+      mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20,
+    }
+
+  },
 
 }
