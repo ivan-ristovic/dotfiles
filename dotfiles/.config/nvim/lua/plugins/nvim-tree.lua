21f94f9099bd0bfd3181ab9015e16da29c2a9719
Ivan Ristovic <work@ristovic.net>
2024-11-29 22:33:52 +0100

    [nvim] load nvim-tree early to avoid nil values during config

--- dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
@@ -39,58 +39,58 @@ return {
     'nvim-tree/nvim-tree.lua',
     requires = { 'nvim-tree/nvim-web-devicons' },
     lazy = false,
-
-    opts = {
-      on_attach = on_attach,
-      update_focused_file = {
-        enable = true,
-        update_cwd = true,
-      },
-      renderer = {
-        root_folder_modifier = ":t",
-        icons = {
-          glyphs = {
-            default = "",
-            symlink = "",
-            folder = {
-              arrow_open = "",
-              arrow_closed = "",
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-              symlink = "",
-              symlink_open = "",
+    init = function()
+        require("nvim-tree").setup({
+            on_attach = on_attach,
+            update_focused_file = {
+              enable = true,
+              update_cwd = true,
             },
-            git = {
-              unstaged = "",
-              staged = "S",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "U",
-              deleted = "",
-              ignored = "◌",
+            renderer = {
+              root_folder_modifier = ":t",
+              icons = {
+                glyphs = {
+                  default = "",
+                  symlink = "",
+                  folder = {
+                    arrow_open = "",
+                    arrow_closed = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                  },
+                  git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌",
+                  },
+                },
+              },
             },
-          },
-        },
-      },
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        icons = {
-          hint = "",
-          info = "",
-          warning = "",
-          error = "",
-        },
-      },
-      view = {
-        width = 30,
-        --[[ height = 30, ]]
-        side = "left",
-      },
-    },  -- end opts
-
+            diagnostics = {
+              enable = true,
+              show_on_dirs = true,
+              icons = {
+                hint = "",
+                info = "",
+                warning = "",
+                error = "",
+              },
+            },
+            view = {
+              width = 30,
+              --[[ height = 30, ]]
+              side = "left",
+            },
+        })
+    end,
     keys = {
        { "<leader>t", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" },
        { "<leader>e", "<cmd>NvimTreeFocus<cr>", desc = "Focus file explorer" },
