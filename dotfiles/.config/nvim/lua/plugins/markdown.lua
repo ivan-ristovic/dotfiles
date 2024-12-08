005e3fd2b05b754bfa67834af66c1e7b7d7fc3cd
Ivan Ristovic <work@ristovic.net>
2024-12-08 12:37:44 +0100

    [nvim] add markdown plugin

---
+++ dotfiles/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+    },
+    opts = {
+      heading = {
+        backgrounds = {
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH2Bg',
+          'RenderMarkdownH2Bg',
+        },
+      }
+    },
+  }
+}
