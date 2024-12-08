687c471a31cce6792badd238a3c0ddc59a36e8e7
Ivan Ristovic <work@ristovic.net>
2024-12-08 13:35:17 +0100

    [nvim] add gitsigns

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -124,6 +124,13 @@ return {
         }
       end
 
+      -- signs
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
     end     -- end config
   }
 
