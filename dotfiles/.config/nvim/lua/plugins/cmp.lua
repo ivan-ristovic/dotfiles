be83c3ceadfa87fe1f17de9f7a4821a3c74866b3
Ivan Ristovic <work@ristovic.net>
2025-04-13 23:29:03 +0200

    [nvim] better abort comp keybind

--- dotfiles/.config/nvim/lua/plugins/cmp.lua
+++ dotfiles/.config/nvim/lua/plugins/cmp.lua
@@ -86,7 +86,7 @@ return {
           ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
           ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-          ["<C-e>"] = cmp.mapping {
+          ["<C-l>"] = cmp.mapping {
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
           },
