468de12189a3e7d87caa92c8bf1d5bf3d0b8c8d6
Ivan Ristovic <work@ristovic.net>
2025-07-20 22:35:25 +0200

    [nvim] fold

--- dotfiles/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/.config/nvim/lua/plugins/misc.lua
@@ -94,5 +94,28 @@ return {
     },
   },
 
+  -- Fold enhancements
+  {
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldmethod = "expr"
+
+      -- :h vim.treesitter.foldexpr()
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+      -- ref: https://github.com/neovim/neovim/pull/20750
+      vim.opt.foldtext = ""
+      vim.opt.fillchars:append("fold: ")
+
+      -- Open all folds by default, zm is not available
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
+
 }
 
