3785f4e2584a47295992378cc5a296b9424ba65d
Ivan Ristovic <work@ristovic.net>
2025-09-27 20:01:20 +0200

    [nvim] use caps for spider motions

--- dotfiles/.config/nvim/lua/plugins/coding.lua
+++ dotfiles/.config/nvim/lua/plugins/coding.lua
@@ -118,10 +118,10 @@ return {
     "chrisgrieser/nvim-spider",
     lazy = true,
     keys = {
-		{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
-		{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
-		{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
-	},
+      { "W", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "E", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "B", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+    },
   },
 
 }
