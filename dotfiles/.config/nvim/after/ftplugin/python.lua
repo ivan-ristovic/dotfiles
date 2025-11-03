23c22b5d3a07da4fc6e847e2d3947819ae9b9bc9
Ivan Ristovic <work@ristovic.net>
2025-11-03 21:12:25 +0100

    [nvim] ftplugin: python

---
+++ dotfiles/.config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,41 @@
+-- python standard
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+
+-- formatters prescribe comments being separated by two spaces
+vim.opt_local.listchars:append { multispace = " " }
+vim.opt_local.formatoptions:append("r") -- `<CR>` in insert mode
+
+--------------------------------------------------------------------------------
+
+-- VIRTUAL_ENV – auto-set
+vim.defer_fn(function()
+	local venv = (vim.uv.cwd() or "") .. "/.venv"
+	if vim.uv.fs_stat(venv) then vim.env.VIRTUAL_ENV = venv end
+end, 1)
+
+--------------------------------------------------------------------------------
+-- ABBREVIATIONS
+local abbr = require("config.utils").bufAbbrev
+abbr("true", "True")
+abbr("false", "False")
+abbr("//", "#")
+abbr("--", "#")
+abbr("null", "None")
+abbr("nil", "None")
+abbr("none", "None")
+abbr("trim", "strip")
+abbr("function", "def")
+
+--------------------------------------------------------------------------------
+-- KEYMAPS
+
+local bkeymap = require("config.utils").bufKeymap
+
+bkeymap("n", "<leader>ci", function()
+	vim.lsp.buf.code_action {
+		filter = function(a) return a.title:find("import") ~= nil end,
+		apply = true,
+	}
+end, { desc = "Import word under cursor" })
+
