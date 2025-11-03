19f499d708f94e980817a0cc99749a7b79d1d21e
Ivan Ristovic <work@ristovic.net>
2025-11-03 21:12:08 +0100

    [nvim] add utils

---
+++ dotfiles/.config/nvim/lua/config/utils.lua
@@ -0,0 +1,19 @@
+local M = {}
+--------------------------------------------------------------------------------
+
+---sets `buffer`, `silent` and `nowait` to true
+---@param mode string|string[]
+---@param lhs string
+---@param rhs string|function
+---@param opts? {desc?: string, unique?: boolean, buffer?: number|boolean, remap?: boolean, silent?:boolean, nowait?: boolean}
+function M.bufKeymap(mode, lhs, rhs, opts)
+	opts = vim.tbl_extend("force", { buffer = true, silent = true, nowait = true }, opts or {})
+	vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+---@param text string
+---@param replace string
+function M.bufAbbrev(text, replace) vim.keymap.set("ia", text, replace, { buffer = true }) end
+
+--------------------------------------------------------------------------------
+return M
