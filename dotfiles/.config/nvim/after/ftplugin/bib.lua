73edbac4e4df4be01a0ab209f6e338075a1013bf
Ivan Ristovic <work@ristovic.net>
2025-11-04 21:05:31 +0100

    [nvim] ftplugin: bibtex

---
+++ dotfiles/.config/nvim/after/ftplugin/bib.lua
@@ -0,0 +1,38 @@
+-- Source:
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/after/ftplugin/bib.lua
+--------------------------------------------------------------------------------
+
+-- scroll down on reading a file
+vim.schedule(function()
+	if vim.b.bib_did_scroll then return end
+	vim.cmd.normal { "G", bang = true }
+	vim.b.bib_did_scroll = true
+end)
+
+-- `%` not actually a comment character, just convention of some programs
+-- https://tex.stackexchange.com/questions/261261/are-comments-discouraged-in-a-bibtex-file
+vim.bo.commentstring = "% %s"
+vim.opt_local.formatoptions:append { r = true }
+
+--------------------------------------------------------------------------------
+
+-- since treesitter has not symbol support for bibtex, we just use a small
+-- function to search the buffer for citekeys
+vim.schedule(function()
+	vim.keymap.set("n", "fc", function()
+		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+		local citekeys = vim.iter(lines)
+			:filter(function(line) return line:find("^@") end)
+			:map(function(line) return line:match("^@.*{(.*),") end)
+			:totable()
+		vim.ui.select(citekeys, {
+			prompt = "Select citekey:",
+			format_item = function(citekey) return "@" .. citekey end,
+			kind = "bibtex.citekey-search",
+		}, function(citekey)
+			if not citekey then return end
+			vim.fn.search(citekey .. ",")
+		end)
+	end, { buffer = true, desc = "[F]ind [C]itations" })
+end)
+
