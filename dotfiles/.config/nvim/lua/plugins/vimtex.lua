bcb05ae45f9b5bcb14ee2d7e8ee0e78517adff7e
Ivan Ristovic <work@ristovic.net>
2025-12-10 00:26:35 +0100

    [vim] fix vimtex view method

--- dotfiles/.config/nvim/lua/plugins/vimtex.lua
+++ dotfiles/.config/nvim/lua/plugins/vimtex.lua
@@ -20,7 +20,7 @@ return {
         },
       }
 
-      vim.g.vimtex_view_method = 'okular'
+      vim.g.vimtex_view_method = 'general'
       vim.g.vimtex_view_general_viewer = 'okular'
       vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 
