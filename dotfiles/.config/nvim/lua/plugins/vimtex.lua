c7b347cb7d8a42e899df42ad023e73e75293b36b
Ivan Ristovic <work@ristovic.net>
2025-12-19 15:15:11 +0100

    [nvim] update vimtex config

--- dotfiles/.config/nvim/lua/plugins/vimtex.lua
+++ dotfiles/.config/nvim/lua/plugins/vimtex.lua
@@ -21,7 +21,7 @@ return {
       }
 
       vim.g.vimtex_view_method = 'general'
-      vim.g.vimtex_view_general_viewer = 'okular'
+      --[[ vim.g.vimtex_view_general_viewer = 'okular' ]]
       vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 
       -- Disable conceal
