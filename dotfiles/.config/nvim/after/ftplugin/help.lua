3fdfc895a0bae2be4de8a9eed7d16d4c0cc0cc66
Ivan Ristovic <work@ristovic.net>
2026-01-24 20:50:15 +0100

    [nvim] add ftplugin navigation

---
+++ dotfiles/.config/nvim/after/ftplugin/help.lua
@@ -0,0 +1,22 @@
+-- Source:
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/after/ftplugin/
+--------------------------------------------------------------------------------
+
+-- OPTIONS
+vim.diagnostic.enable(false, { bufnr = 0 })
+vim.opt_local.colorcolumn = ""
+vim.opt_local.wrap = true
+
+-- KEYMAPS
+local bkeymap = require("config.utils").bufKeymap
+
+-- `:bwipeout` so it isn't saved in oldfiles
+bkeymap("n", "q", vim.cmd.bwipeout, { desc = "Quit" })
+
+-- `gO` opens the heading-selection in vim help files. 
+-- (Only for txt-help files, so lazy-generated markdown help files are not affected.)
+local txtHelpFile = vim.api.nvim_buf_get_name(0):match("%.(%w+)$") == "txt"
+if txtHelpFile then
+	bkeymap("n", "gs", "gO", { remap = true })
+	bkeymap("n", "gd", "<C-]>", { desc = "Goto definition" })
+end
