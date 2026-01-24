0b8d95836281401f71881bf7e5ee3cf101ab01ee
Ivan Ristovic <work@ristovic.net>
2026-01-24 20:52:31 +0100

    [nvim] add ftplugin log

---
+++ dotfiles/.config/nvim/after/ftplugin/log.lua
@@ -0,0 +1,10 @@
+-- Source:
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/after/ftplugin/
+--------------------------------------------------------------------------------
+
+-- Go to tail of the log
+if vim.b.already_scrolled_logfile then return end
+vim.defer_fn(function()
+	vim.cmd.normal { "G", bang = true }
+	vim.b.already_scrolled_logfile = true
+end, 1)
